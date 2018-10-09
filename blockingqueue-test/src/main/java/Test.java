9500a86017e81ba8d8f1ce8292e575528d443af7
司云飞 <siyunfei@qiyi.com>
2018-10-09 21:28:12 +0800

    update

--- blockingqueue-test/src/main/java/Test.java
+++ blockingqueue-test/src/main/java/Test.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -5,29 +6,41 @@ import java.util.concurrent.BlockingQueue;
  * Created by siyunfei on 2018/10/9.
  */
 public class Test {
-    BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(10);
+    static Random random = new Random();
+
+    static BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(10);
 
     public static void main(String[] args) {
-
+        new Producer().start();
+        new Consumer().start();
     }
 
-    class Producer extends Thread{
+    static class Producer extends Thread {
         @Override
         public void run() {
             while (true) {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(random.nextInt(1000));
+                    queue.put(2);
+                    System.out.println("produce 2...");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                queue.offer(2);
             }
         }
     }
-    class Consumer extends Thread {
+
+    static class Consumer extends Thread {
         @Override
         public void run() {
-            
+            while (true) {
+                try {
+                    Thread.sleep(random.nextInt(1000));
+                    System.out.println("consume " + queue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
