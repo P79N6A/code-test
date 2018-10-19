13092e8b97b632f2eacc42ec5de356f1b164178e
司云飞 <siyunfei@qiyi.com>
2018-10-19 16:44:53 +0800

    update couchbase-test

--- couchbase-test/src/main/java/CBTest.java
+++ couchbase-test/src/main/java/CBTest.java
@@ -1,3 +1,4 @@
+import com.couchbase.client.CouchbaseClient;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.CouchbaseCluster;
@@ -5,9 +6,13 @@ import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.json.JsonArray;
 import com.couchbase.client.java.document.json.JsonObject;
 
+import java.net.URI;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CBTest {
     public static void main(String[] args) {
-        Cluster cluster = CouchbaseCluster.create("localhost");
+        /*Cluster cluster = CouchbaseCluster.create("localhost");
         cluster.authenticate("admin", "123456");
         Bucket bucket = cluster.openBucket("mybucket");
 
@@ -19,6 +24,17 @@ public class CBTest {
         bucket.upsert(JsonDocument.create("stu_xiaobai", jsonObject));
         System.out.println(bucket.get("stu_xiaobai"));
         bucket.close();
-        cluster.disconnect();
+        cluster.disconnect();*/
+
+        List<URI> uris = new LinkedList<URI>();
+        uris.add(URI.create("http://127.0.0.1:8091/pools"));
+        CouchbaseClient client = null;
+        try {
+            client = new CouchbaseClient(uris, "mybucket", "123456");
+        } catch (Exception e) {
+            System.err.println("Error connecting to Couchbase: "
+                    + e.getMessage());
+            System.exit(0);
+        }
     }
 }
