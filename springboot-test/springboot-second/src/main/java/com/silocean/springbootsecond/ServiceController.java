bc7a45191013cd15d6e4ad97a5f327da06e7f97d
司云飞 <siyunfei@qiyi.com>
2018-11-19 09:04:30 +0800

    update

---
+++ springboot-test/springboot-second/src/main/java/com/silocean/springbootsecond/ServiceController.java
@@ -0,0 +1,24 @@
+package com.silocean.springbootsecond;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author siyunfei
+ * 2018/11/17 2:51 PM
+ */
+@RestController
+@RequestMapping("child-service")
+public class ServiceController {
+    private static final Logger logger = LoggerFactory.getLogger(ServiceController.class);
+
+    @RequestMapping("/hello")
+    public String apiGet() {
+        logger.info("======child service======");
+        return "hello child";
+    }
+}
